e8f97f3e87f2765febfc0c61a63b3c373f93aaff
Samuel Barbosa <samuca192silva@hotmail.com>
2023-03-10 15:10:16 -0300

    feat - Project init

---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+    meta: {
+      title: 'Login',
+    },
+  },
+  {
+    path: '/about',
+    name: 'about',
+    meta: {
+      title: 'Sobre',
+    },
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title;
+  }
+
+  return next();
+});
+
+export default router;
